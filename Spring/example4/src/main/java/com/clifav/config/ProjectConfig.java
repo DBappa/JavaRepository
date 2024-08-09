7e1a6c6c5fe209c5e8f9165fe2f00110b2e00cb3
DBappa <bappadityadutta2024@gmail.com>
2024-08-09 09:32:32 +0530

    Primary Annotation

---
+++ Spring/example4/src/main/java/com/clifav/config/ProjectConfig.java
@@ -0,0 +1,45 @@
+package com.clifav.config;
+
+
+import com.clifav.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean(name="bmw")
+    Vehicle vehicle1(){
+        var vehicle=new Vehicle();
+        vehicle.setName("BMW");
+        return vehicle;
+    }
+
+//    @Bean
+//    String hello(){
+//        return "Hello World";
+//    }
+//
+//    @Bean
+//    Integer number(){
+//        return 16;
+//    }
+
+    @Bean(value = "audi")
+    @Primary
+    Vehicle vehicle2(){
+        var vehicle=new Vehicle();
+        vehicle.setName("Audi");
+        return vehicle;
+    }
+
+    @Bean("mercedez")
+    Vehicle vehicle3(){
+        var vehicle3=new Vehicle();
+        vehicle3.setName("Mercedez");
+        return vehicle3;
+    }
+
+
+}
